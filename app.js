d12aa8dcf5fbdea4cd82c7c88154a5b80d96265c
AkshayO3 <akshaybhandari020@gmail.com>
2023-06-01 23:20:12 +0530

    Challenge 20 complete

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _=require("lodash");
-const {lowerCase} = require("lodash");
+const Str = require("@supercharge/strings");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,7 +17,6 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 let posts=[];
-
 app.get("/",function(req,res){
   res.render("home",{startingC:homeStartingContent,po:posts});
 })
@@ -31,9 +30,10 @@ app.get("/compose",function(req,res){
   res.render("compose");
 })
 app.post("/compose",function(req,res){
-const post={
+  const trunstr = req.body.content.substring(0, 100)+'...'
+  const post={
   title:req.body.blog,
-  content:req.body.content
+  content:trunstr
 };
   posts.push(post);
   res.redirect("/")
